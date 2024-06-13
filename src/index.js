af4296b2d8ccda0dc235a0489a8db6588819f2a8
Felipe Alves <felipealexandre1408@gmail.com>
2024-06-13 16:12:34 -0300

    Feat: adding main menu and user can search for the temperature by giving its cityname.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 const venom = require('venom-bot');
 const axios = require('axios');
 
-
-
-
 venom.create({
     session: 'session-name'
     })
     .then((client) => {
         start(client);
-        
+
         process.on('SIGINT', () => {
             client.close().then(() => {
                 console.log('Bot fechado.');
@@ -19,7 +16,7 @@ venom.create({
                 process.exit(1);
             });
         });
-    
+
         process.on('SIGTERM', () => {
             client.close().then(() => {
                 console.log('Bot fechado.');
@@ -30,26 +27,26 @@ venom.create({
             });
         });
 
-
     })
     .catch((error) => {
         console.log(error);
     });
 
 const start = (client) => {
-
     let users = {};
 
     client.onMessage((message) => {
         const user = message.from;
 
         if (!users[user]) {
-        users[user] = {
-            stage: 0,
-            order: null,
-            address: null,
-            payment: null,
-        };
+            users[user] = {
+                stage: 0,
+                order: null,
+                address: null,
+                payment: null,
+                selectedMenu: null,
+                cityName: null,
+            };
         }
 
         handleStage(client, message, users[user]);
@@ -58,49 +55,67 @@ const start = (client) => {
 
 async function handleStage(client, message, user) {
     switch (user.stage) {
-    case 0:
-        client.sendText(message.from, 'Olá! Aguarde um momento enquanto buscamos a temperatura de recife.');
-        try {
-            const cityName = "recife";
-            const apiKey = `456165af1a0e7545d7d46530a7190936`;
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-                            
+        case 0:
+            client.sendText(message.from, 'Olá! Escolha uma opção:\n1: Saber mais informações sobre este bot\n2: Ver a temperatura em uma cidade\n3: Fazer pedido de pizza');
+            user.stage = 1;
+            break;
+        case 1:
+            user.selectedMenu = message.body;
+            switch (user.selectedMenu) {
+                case '1':
+                    client.sendText(message.from, 'Olá! Sou um bot do Secret no WhatsApp. Em breve estarei recebendo novas atualizações para melhor atendê-lo.');
+                    user.stage = 0;
+                    break;
+                case '2':
+                    client.sendText(message.from, 'Por favor, informe o nome da cidade que você gostaria de saber a temperatura.');
+                    user.stage = 2;
+                    break;
+                case '3':
+                    client.sendText(message.from, 'Qual pizza você gostaria de pedir?');
+                    user.stage = 4;
+                    break;
+                default:
+                    client.sendText(message.from, 'Opção inválida. Por favor, escolha uma opção válida:\n1: Saber mais informações sobre este bot\n2: Ver a temperatura em uma cidade\n3: Fazer pedido de pizza');
+                    user.stage = 1;
+                    break;
+            }
+            break;
+        case 2:
+            user.cityName = message.body;
+            client.sendText(message.from, `Aguarde um momento enquanto buscamos a temperatura de ${user.cityName}.`);
+            try {
+                const apiKey = `456165af1a0e7545d7d46530a7190936`;
+                const url = `https://api.openweathermap.org/data/2.5/weather?q=${user.cityName}&appid=${apiKey}`;
 
+                const response = await axios.get(url);
+                const data = response.data;
+                const tempCelsius = (data.main.temp - 273.15).toFixed(2);
 
-            const response = await axios.get(url);
-            const data = response.data;
-            const tempCelsius = (data.main.temp - 273.15).toFixed(2);
-            
-
-            client.sendText(message.from, `A temperatura atual em Recife é ${tempCelsius}°C.`);
-            client.sendText(message.from, 'Qual pizza você gostaria de pedir?');
-            user.stage++;
-        } catch (error) {
-            client.sendText(message.from, 'Desculpe, houve um problema ao buscar a temperatura. Tente novamente mais tarde.');
+                client.sendText(message.from, `A temperatura atual em ${user.cityName.toUpperCase()} é ${tempCelsius}°C.`);
+                user.stage = 0;
+            } catch (error) {
+                client.sendText(message.from, 'Desculpe, houve um problema ao buscar a temperatura. Tente novamente mais tarde.');
+                user.stage = 0;
+            }
+            break;
+        case 4:
+            user.order = message.body;
+            client.sendText(message.from, 'Qual é o seu endereço para entrega?');
+            user.stage = 5;
+            break;
+        case 5:
+            user.address = message.body;
+            client.sendText(message.from, 'Quanto você tem para pagar pelo pedido?');
+            user.stage = 6;
+            break;
+        case 6:
+            user.payment = message.body;
+            client.sendText(message.from, `Resumo do pedido:\n- Pizza: ${user.order}\n- Endereço: ${user.address}\n- Pagamento: ${user.payment}\n\nObrigado pelo seu pedido!`);
             user.stage = 0;
-        }
-        break;
-    case 1:
-        user.order = message.body;
-        client.sendText(message.from, 'Qual é o seu endereço para entrega?');
-        user.stage++;
-        break;
-    case 2:
-        user.address = message.body;
-        client.sendText(message.from, 'Quanto você tem para pagar pelo pedido?');
-        user.stage++;
-        break;
-    case 3:
-        user.payment = message.body;
-        client.sendText(
-        message.from,
-        `Resumo do pedido:\n- Pizza: ${user.order}\n- Endereço: ${user.address}\n- Pagamento: ${user.payment}\n\nObrigado pelo seu pedido!`
-        );
-        user.stage = 0;
-        break;
-    default:
-        client.sendText(message.from, 'Desculpe, houve um erro no atendimento.');
-        user.stage = 0;
-        break;
+            break;
+        default:
+            client.sendText(message.from, 'Desculpe, houve um erro no atendimento.');
+            user.stage = 0;
+            break;
     }
 }
